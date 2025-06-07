3c399604f19f6cfe574db820c4ea70b2bf645907
houssam bouajjioune <houssambouajjioune1998@gmail.com>
2025-06-07 01:34:01 +0100

    fix: remove redundant interface definition for LayoutProps in Layout.tsx

--- alx-project-0x03/components/layouts/Layout.tsx
+++ alx-project-0x03/components/layouts/Layout.tsx
@@ -4,11 +4,6 @@ import Header from "./Header";
 import { LayoutProps } from "@/interface";
 
 
-
-interface LayoutProps {
-  children: ReactNode;
-}
-
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <>
