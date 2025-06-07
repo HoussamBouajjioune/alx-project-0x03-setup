80adc802e2bc7319ec4782a2ad37a52027443c7f
houssam bouajjioune <houssambouajjioune1998@gmail.com>
2025-06-07 01:27:11 +0100

    fix: remove duplicate interface definition for PageRouteProps in index.tsx

--- alx-project-0x03/interface/index.tsx
+++ alx-project-0x03/interface/index.tsx
@@ -1,18 +1,15 @@
-// From pages/index.tsx
+import { ReactNode } from "react";
+
 export interface PageRouteProps {
   pageRoute: string;
 }
 
-// From components/common/Button.tsx
 export interface ButtonProps {
   buttonLabel: string;
   buttonSize?: string;
   buttonBackgroundColor?: 'red' | 'blue' | 'orange' | 'green';
   action?: () => void;
 }
-
-// From components/layouts/Layout.tsx
-import { ReactNode } from "react";
 
 export interface LayoutProps {
   children: ReactNode;
