07a180ca38fd95437d4125da67a7d93c78dbdf6a
houssam bouajjioune <houssambouajjioune1998@gmail.com>
2025-06-07 01:00:15 +0100

    commit message goes here

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -1,13 +1,9 @@
-// pages/index.tsx
-import Layout from "@/components/layouts/Layout";
-
-export default function Home() {
+const Home = () => {
   return (
-    <Layout>
-      <div className="container mx-auto px-4">
-        <h1 className="text-4xl font-bold mt-10">Welcome to Splash App</h1>
-        <p className="mt-4 text-gray-600">Start exploring the platform today!</p>
-      </div>
-    </Layout>
+    <div>
+      <h1>Welcome to Splash App</h1>
+    </div>
   );
-}
+};
+
+export default Home;
